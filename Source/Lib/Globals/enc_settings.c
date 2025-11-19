a1d4edb7025343681620cdbd370b3307c2054533
Akatsumekusa <Akatsumekusa@protonmail.com>
2025-11-19 06:07:22 +0000

    exp: `--chroma-distortion-taper` and `--skip-taper`

--- Source/Lib/Globals/enc_settings.c
+++ Source/Lib/Globals/enc_settings.c
@@ -965,6 +965,16 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet *scs) {
     //     return_error = EB_ErrorBadParameter;
     // }
 
+    if (config->chroma_distortion_taper > 1) {
+        SVT_ERROR("Instance %u: chroma-distortion-taper must be between 0 and 1\n", channel_number + 1);
+        return_error = EB_ErrorBadParameter;
+    }
+
+    if (config->skip_taper > 1) {
+        SVT_ERROR("Instance %u: skip-taper must be between 0 and 1\n", channel_number + 1);
+        return_error = EB_ErrorBadParameter;
+    }
+
     if (config->sharp_tx > 1) {
         SVT_ERROR("Instance %u: sharp-tx must be between 0 and 1\n", channel_number + 1);
         return_error = EB_ErrorBadParameter;
@@ -1156,6 +1166,8 @@ EbErrorType svt_av1_set_default_params(EbSvtAv1EncConfiguration *config_ptr) {
     config_ptr->psy_rd                            = 0.5;
     config_ptr->spy_rd                            = 0;
     config_ptr->low_q_taper                       = 0;
+    config_ptr->chroma_distortion_taper           = 0;
+    config_ptr->skip_taper                        = 0;
     config_ptr->sharp_tx                          = 1;
     config_ptr->hbd_mds                           = 0;
     config_ptr->complex_hvs                       = 0;
@@ -1386,6 +1398,14 @@ void svt_av1_print_lib_params(SequenceControlSet *scs) {
 		if (config->low_q_taper) {
             SVT_INFO("SVT [config]: low Q taper \t\t\t\t\t\t\t: %s\n",
                     config->low_q_taper ? "on" : "off");
+        }
+        
+		if (config->chroma_distortion_taper) {
+            SVT_INFO("SVT [config]: chroma distortion taper \t\t\t\t\t: on\n");
+        }
+        
+		if (config->skip_taper) {
+            SVT_INFO("SVT [config]: skip taper \t\t\t\t\t\t\t: on\n");
         }
         
         switch (config->filtering_noise_detection) {
@@ -2307,6 +2327,8 @@ EB_API EbErrorType svt_av1_enc_parse_parameter(EbSvtAv1EncConfiguration *config_
         {"tf-strength", &config_struct->tf_strength},
         {"kf-tf-strength", &config_struct->kf_tf_strength},
         {"noise-norm-strength", &config_struct->noise_norm_strength},
+        {"chroma-distortion-taper", &config_struct->chroma_distortion_taper},
+        {"skip-taper", &config_struct->skip_taper},
         {"fast-decode", &config_struct->fast_decode},
         {"enable-tf", &config_struct->enable_tf},
         {"spy-rd", &config_struct->spy_rd},
